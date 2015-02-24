4be4d17dc5c157990b538ab7fe26058b84829884
unknown <1301504@MC10647.uad.ac.uk>
2015-02-24 16:02:17 +0000

    Added in A Menu class system, for changing and displaying menu states

--- Stable/Player Class.py
+++ Stable/Player Class.py
@@ -10,6 +10,62 @@ DISPLAY_HEIGHT = 480;
 BACK_COLOR = (0,0,255);
 FPS_TARGET = 60;
 
+#             R    G    B
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+DARKGREEN = (  0, 155,   0)
+DARKGRAY  = ( 40,  40,  40)
+BGCOLOR = BLACK
+
+class Menu:
+    spriteSheet = None;
+    image = None;
+    position = [(DISPLAY_WIDTH/2) - 140, 50];
+    MenuRect = [(0, 0, 280, 233)];
+    ChooseChRect = [(0, 175, 575, 195)];
+    
+    def init(self):
+        self.spriteSheet = pygame.image.load("Images/MenuHolder.png").convert();
+        #objSurf.set_colorkey((0,162,232));
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.ChooseChRect[0]));
+        #self.image = self.spriteSheet.subsurface(pygame.Rect(self.MenuRect[0]));
+ 
+    def render(self,displaySurface):
+        displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
+        
+    #will render the text for the Menu
+    def rendertext(self, displaySurface):
+        MenuFont = pygame.font.Font('freesansbold.ttf', 25);
+        StartSurf = MenuFont.render('Start Game', True, WHITE);
+        ChooseChSurf = MenuFont.render('Choose Character', True, WHITE);
+        ExitSurf = MenuFont.render('Exit', True, WHITE);
+        
+        StartRect = StartSurf.get_rect();
+        ChooseChRect = ChooseChSurf.get_rect();
+        ExitRect = ExitSurf.get_rect();
+        
+        StartRect.midtop = (DISPLAY_WIDTH / 2, 65);
+        ChooseChRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 125);
+        ExitRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 215);
+        
+        displaySurface.blit(StartSurf, StartRect);
+        displaySurface.blit(ChooseChSurf, ChooseChRect);
+        displaySurface.blit(ExitSurf, ExitRect);
+    
+    #will render the Character Selection Menu
+    def CharacterChRender(self, displaySurface):
+        displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
+    #will switch UV's to the Character Selections
+    def ChangeToChooseCh(self, displaySurface):
+        self.position = [25, 200];
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.ChooseChRect[0]));
+    #will switch UV's to the Normal Menu's
+    def ChangeToMenu(self, displaySurface):
+        self.position = [(DISPLAY_WIDTH/2) - 140, 50];
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.MenuRect[0]));
+        
 class Player:
     
     spriteSheet = None;
@@ -37,7 +93,7 @@ class Player:
 def main():
         pygame.init();
         init();
-       
+        menu.ChangeToMenu(VIEWPORT);
         while True:
                 update();
                 render();
@@ -56,8 +112,10 @@ def init():
         player = Player();
         player.init();
         
-        
-        
+        global menu;
+        menu = Menu();
+        menu.init();
+
 def update():
         #Update Function
        
@@ -90,6 +148,9 @@ def render():
         #Draw Object
         #VIEWPORT.blit(player_image,player_image.get_rect(topleft = objPos));
         player.render(VIEWPORT);
+        #menu.render(VIEWPORT);
+        #menu.rendertext(VIEWPORT);
+        menu.CharacterChRender(VIEWPORT);
         
         #Updates The Display
         pygame.display.update();
