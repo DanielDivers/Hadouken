f7d7482d6f5d2fb3daa1e7ff3ef495843a2f36c8
unknown <1301504@MC10647.uad.ac.uk>
2015-02-25 14:44:28 +0000

    Added a lot of commentary, Made Ryu Sprites appear in Correct Positions, and tidied up code

--- Stable/Player Class.py
+++ Stable/Player Class.py
@@ -20,46 +20,85 @@ DARKGRAY  = ( 40,  40,  40)
 BGCOLOR = BLACK
 
 class Menu:
+
+    #will contain the whole loaded image
     spriteSheet = None;
     background = None;
+    RyuSprites = None;
     
+    #will contain cut image from UV's
     image = None;
     backImage = None;
+    RyuImage = None;
+    DarkRyuImage = None;
+    FlashRyuImage = None;
     
+    #positions for drawing
+    RyuPosition = ((DISPLAY_WIDTH / 4) - 10, (DISPLAY_HEIGHT / 2) - 80);
+    DarkRyuPosition = (((3*(DISPLAY_WIDTH / 4) - 30), (DISPLAY_HEIGHT / 2) - 80));
+    FlashRyuPosition = (DISPLAY_WIDTH/2 - 20, 300);
     position = [(DISPLAY_WIDTH/2) - 140, 50];
+    
+    #UV's 
+    RyuImageUV = [(8, 13, 20, 23)];
+    DarkRyuImageUV = [(7, 82, 20, 23)];
+    FlashRyuImageUV = [(7, 152, 20, 23)]
     MenuRect = [(0, 0, 280, 233)];
     ChooseChRect = [(0, 175, 575, 195)];
     BackgroundRect = [(0, 0, 640, 480)];
+    
+    #internal bools for switching states
     StartMenu = True;
     CharacterSelectionMenu = False;
     
     def init(self):
+        #load in the images
         self.background = pygame.image.load("Images/Background.png").convert();
         self.spriteSheet = pygame.image.load("Images/MenuHolder.png").convert();
+        self.RyuSprites  = pygame.image.load("Images/Ryu.png").convert();
+        
+        #make these colours not be drawn for the corresponding Image
+        self.RyuSprites.set_colorkey((255,0,255));
         self.spriteSheet.set_colorkey((255,0,255));
-        #objSurf.set_colorkey((0,162,232));
+        
+        #Cut the loaded images with Specified UV's and store it
         self.image = self.spriteSheet.subsurface(pygame.Rect(self.MenuRect[0]));
         self.backImage = self.background.subsurface(pygame.Rect(self.BackgroundRect[0]));
+        self.RyuImage = self.RyuSprites.subsurface(pygame.Rect(self.RyuImageUV[0]));
+        self.DarkRyuImage = self.RyuSprites.subsurface(pygame.Rect(self.DarkRyuImageUV[0]));
+        self.FlashRyuImage = self.RyuSprites.subsurface(pygame.Rect(self.FlashRyuImageUV[0]));
+        
+        #Scale the Sprites
+        self.RyuImage = pygame.transform.scale2x(self.RyuImage);
+        self.DarkRyuImage = pygame.transform.scale2x(self.DarkRyuImage)
+        self.FlashRyuImage = pygame.transform.scale2x(self.FlashRyuImage)
  
+    #renders the main menu
     def renderMenu(self,displaySurface):
         displaySurface.blit(self.backImage,self.backImage.get_rect(topleft = (0,0)));
         displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
         
-    #will render the text for the Menu
+    #will render the text for the main menu
     def renderMenutext(self, displaySurface):
+        #Set up Font Style
         MenuFont = pygame.font.Font('freesansbold.ttf', 25);
+        
+        #Set the text to be written
         StartSurf = MenuFont.render('Start Game', True, WHITE);
         ChooseChSurf = MenuFont.render('Choose Character', True, WHITE);
         ExitSurf = MenuFont.render('Exit', True, WHITE);
         
+        #make rectangles 
         StartRect = StartSurf.get_rect();
         ChooseChRect = ChooseChSurf.get_rect();
         ExitRect = ExitSurf.get_rect();
         
+        #set up Coords for display
         StartRect.midtop = (DISPLAY_WIDTH / 2, 65);
         ChooseChRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 125);
         ExitRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 215);
         
+        #Draw the text
         displaySurface.blit(StartSurf, StartRect);
         displaySurface.blit(ChooseChSurf, ChooseChRect);
         displaySurface.blit(ExitSurf, ExitRect);
@@ -69,21 +108,32 @@ class Menu:
         displaySurface.blit(self.backImage,self.backImage.get_rect(topleft = (0,0)));
         displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
         
+        #Displays Ryu Sprites
+        displaySurface.blit(self.RyuImage,self.RyuImage.get_rect(topleft = self.RyuPosition));
+        displaySurface.blit(self.DarkRyuImage ,self.DarkRyuImage.get_rect(topleft = self.DarkRyuPosition));
+        displaySurface.blit(self.FlashRyuImage,self.FlashRyuImage.get_rect(topleft = self.FlashRyuPosition));
+        
     #will render the Character Selection Menu's text
     def renderCharacterChText(self, displaySurface):
+        #Set up Font Style
         MenuFont = pygame.font.Font('freesansbold.ttf', 25);
+        
+        #Set the text to be written
         RyuSurf = MenuFont.render('Ryu', True, WHITE);
         DarkRyuSurf = MenuFont.render('Dark Ryu', True, WHITE);
         FlashRyuSurf = MenuFont.render('Flash Ryu', True, WHITE);
         
+        #make rectangles 
         RyuRect = RyuSurf.get_rect();
         DarkRyuRect = DarkRyuSurf.get_rect();
         FlashRyuRect = FlashRyuSurf.get_rect();
         
+        #set up Coords for display
         RyuRect.midtop = (DISPLAY_WIDTH / 4, DISPLAY_HEIGHT/2 - 25);
         DarkRyuRect.midtop = (3*(DISPLAY_WIDTH/4) - 20, DISPLAY_HEIGHT/2 - 25);
         FlashRyuRect.midtop = (DISPLAY_WIDTH / 2 - 5, 3*(DISPLAY_HEIGHT/4) - 7);
         
+        #Draw the text
         displaySurface.blit(RyuSurf, RyuRect);
         displaySurface.blit(DarkRyuSurf, DarkRyuRect);
         displaySurface.blit(FlashRyuSurf, FlashRyuRect);
--- Stable/wormy.py
+++ Stable/wormy.py
@@ -1,286 +1,127 @@
-# Wormy (a Nibbles clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
+class Menu:
 
-#KRT 14/06/2012 modified Start Screen and Game Over screen to cope with mouse events
-#KRT 14/06/2012 Added a non-busy wait to Game Over screen to reduce processor loading from near 100%
-
-#
-# Modified by Grant Clarke
-# g.clarke@abertay.ac.uk
-#
-#
-
-import random, pygame, sys
-from pygame.locals import *
-
-FPS = 15
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 480
-CELLSIZE = 20
-assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
-assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
-CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
-CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
-
-pygame.init()
-effect = pygame.mixer.Sound('hadoukenogg.ogg');
-
-#             R    G    B
-WHITE     = (255, 255, 255)
-BLACK     = (  0,   0,   0)
-RED       = (255,   0,   0)
-GREEN     = (  0, 255,   0)
-DARKGREEN = (  0, 155,   0)
-DARKGRAY  = ( 40,  40,  40)
-BGCOLOR = BLACK
-
-UP = 'up'
-DOWN = 'down'
-LEFT = 'left'
-RIGHT = 'right'
-
-HEAD = 0 # syntactic sugar: index of the worm's head
-
-#
-# GLOBAL VARIABLES used by multiple functions
-#
-# Set a random start point.
-gStartx = random.randint(5, CELLWIDTH - 6)
-gStarty = random.randint(5, CELLHEIGHT - 6)
-gWormCoords = [{'x': gStartx,     'y': gStarty},
-              {'x': gStartx - 1, 'y': gStarty},
-              {'x': gStartx - 2, 'y': gStarty}]
-gDirection = RIGHT
-
-
-
-gApple = {'x': 0, 'y': 0}
-
-#
-#
-#
-def main():
-    init()
-    showStartScreen()
-    while True:
-        runGame()
-        showGameOverScreen()
-
-def init():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT
-
+    #will contain the whole loaded image
+    spriteSheet = None;
+    background = None;
+    RyuSprites = None;
     
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
-    pygame.display.set_caption('Wormy')
+    #will contain cut image from UV's
+    image = None;
+    backImage = None;
+    RyuImage = None;
+    DarkRyuImage = None;
+    FlashRyuImage = None;
     
+    #positions for drawing
+    RyuPosition = ((DISPLAY_WIDTH / 4) - 10, (DISPLAY_HEIGHT / 2) - 80);
+    DarkRyuPosition = (((3*(DISPLAY_WIDTH / 4) - 30), (DISPLAY_HEIGHT / 2) - 80));
+    FlashRyuPosition = (DISPLAY_WIDTH/2 - 20, 300);
+    position = [(DISPLAY_WIDTH/2) - 140, 50];
     
-def runGame():
-    game_init()    
-    while True: # main game loop
-        game_over = game_update()
-        game_render()
-        if game_over == True:
-            return # game over, stop running the game
-
-
-def game_init():
-    global gWormCoords, gDirection, gApple
-
-    # Set a random start point.
-    gStartx = random.randint(5, CELLWIDTH - 6)
-    gStarty = random.randint(5, CELLHEIGHT - 6)
-    gWormCoords = [{'x': gStartx,     'y': gStarty},
-                  {'x': gStartx - 1, 'y': gStarty},
-                  {'x': gStartx - 2, 'y': gStarty}]
-    gDirection = RIGHT
+    #UV's 
+    RyuImageUV = [(8, 13, 20, 23)];
+    DarkRyuImageUV = [(7, 82, 20, 23)];
+    FlashRyuImageUV = [(7, 152, 20, 23)]
+    MenuRect = [(0, 0, 280, 233)];
+    ChooseChRect = [(0, 175, 575, 195)];
+    BackgroundRect = [(0, 0, 640, 480)];
+    
+    #internal bools for switching states
+    StartMenu = True;
+    CharacterSelectionMenu = False;
+    
+    def init(self):
+        #load in the images
+        self.background = pygame.image.load("Images/Background.png").convert();
+        self.spriteSheet = pygame.image.load("Images/MenuHolder.png").convert();
+        self.RyuSprites  = pygame.image.load("Images/Ryu.png").convert();
         
-    #pygame.mixer.music.load('guile.mp3')
-    #pygame.mixer.music.play(0)
+        #make these colours not be drawn for the corresponding Image
+        self.RyuSprites.set_colorkey((255,0,255));
+        self.spriteSheet.set_colorkey((255,0,255));
+        
+        #Cut the loaded images with Specified UV's and store it
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.MenuRect[0]));
+        self.backImage = self.background.subsurface(pygame.Rect(self.BackgroundRect[0]));
+        self.RyuImage = self.RyuSprites.subsurface(pygame.Rect(self.RyuImageUV[0]));
+        self.DarkRyuImage = self.RyuSprites.subsurface(pygame.Rect(self.DarkRyuImageUV[0]));
+        self.FlashRyuImage = self.RyuSprites.subsurface(pygame.Rect(self.FlashRyuImageUV[0]));
+        
+        #Scale the Sprites
+        self.RyuImage = pygame.transform.scale2x(self.RyuImage);
+        self.DarkRyuImage = pygame.transform.scale2x(self.DarkRyuImage)
+        self.FlashRyuImage = pygame.transform.scale2x(self.FlashRyuImage)
+ 
+    #renders the main menu
+    def renderMenu(self,displaySurface):
+        displaySurface.blit(self.backImage,self.backImage.get_rect(topleft = (0,0)));
+        displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
+        
+    #will render the text for the main menu
+    def renderMenutext(self, displaySurface):
+        #Set up Font Style
+        MenuFont = pygame.font.Font('freesansbold.ttf', 25);
+        
+        #Set the text to be written
+        StartSurf = MenuFont.render('Start Game', True, WHITE);
+        ChooseChSurf = MenuFont.render('Choose Character', True, WHITE);
+        ExitSurf = MenuFont.render('Exit', True, WHITE);
+        
+        #make rectangles 
+        StartRect = StartSurf.get_rect();
+        ChooseChRect = ChooseChSurf.get_rect();
+        ExitRect = ExitSurf.get_rect();
+        
+        #set up Coords for display
+        StartRect.midtop = (DISPLAY_WIDTH / 2, 65);
+        ChooseChRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 125);
+        ExitRect.midtop = (DISPLAY_WIDTH / 2, StartRect.height + 215);
+        
+        #Draw the text
+        displaySurface.blit(StartSurf, StartRect);
+        displaySurface.blit(ChooseChSurf, ChooseChRect);
+        displaySurface.blit(ExitSurf, ExitRect);
     
-    
-    # Start the gApple in a random place.
-    gApple = getRandomLocation();
-    
-
-
-
-def game_update():
-    global gWormCoords, gDirection, gApple
-
-    for event in pygame.event.get(): # event handling loop
-        if event.type == QUIT:
-            terminate()
-        elif event.type == KEYDOWN:
-            if (event.key == K_LEFT or event.key == K_a) and gDirection != RIGHT:
-                gDirection = LEFT
-                effect.play()
-                
-            elif (event.key == K_RIGHT or event.key == K_d) and gDirection != LEFT:
-                gDirection = RIGHT
-                effect.play()
-            elif (event.key == K_UP or event.key == K_w) and gDirection != DOWN:
-                gDirection = UP
-                effect.play()
-            elif (event.key == K_DOWN or event.key == K_s) and gDirection != UP:
-                gDirection = DOWN
-                effect.play()
-            elif event.key == K_ESCAPE:
-                terminate()
-
-    # check if the worm has hit itself or the edge
-    if gWormCoords[HEAD]['x'] == -1 or gWormCoords[HEAD]['x'] == CELLWIDTH or gWormCoords[HEAD]['y'] == -1 or gWormCoords[HEAD]['y'] == CELLHEIGHT:
-        return True # game over, return True
-    for wormBody in gWormCoords[1:]:
-        if wormBody['x'] == gWormCoords[HEAD]['x'] and wormBody['y'] == gWormCoords[HEAD]['y']:
-            return True # game over, return True
-
-    # check if worm has eaten an apply
-    if gWormCoords[HEAD]['x'] == gApple['x'] and gWormCoords[HEAD]['y'] == gApple['y']:
-        # don't remove worm's tail segment
-        gApple = getRandomLocation() # set a new gApple somewhere
-    else:
-        del gWormCoords[-1] # remove worm's tail segment
-
-    # move the worm by adding a segment in the gDirection it is moving
-    if gDirection == UP:
-        newHead = {'x': gWormCoords[HEAD]['x'], 'y': gWormCoords[HEAD]['y'] - 1}
-    elif gDirection == DOWN:
-        newHead = {'x': gWormCoords[HEAD]['x'], 'y': gWormCoords[HEAD]['y'] + 1}
-    elif gDirection == LEFT:
-        newHead = {'x': gWormCoords[HEAD]['x'] - 1, 'y': gWormCoords[HEAD]['y']}
-    elif gDirection == RIGHT:
-        newHead = {'x': gWormCoords[HEAD]['x'] + 1, 'y': gWormCoords[HEAD]['y']}
-    gWormCoords.insert(0, newHead)
-    # game is not over, return False
-    return False
-
-def game_render():
-    DISPLAYSURF.fill(BGCOLOR)
-    drawGrid()
-    drawWorm(gWormCoords)
-    drawApple(gApple)
-    drawScore(len(gWormCoords) - 3)
-    pygame.display.update()
-    FPSCLOCK.tick(FPS)
-
-def drawPressKeyMsg():
-    pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
-    pressKeyRect = pressKeySurf.get_rect()
-    pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
-    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
-
-# KRT 14/06/2012 rewrite event detection to deal with mouse use
-def checkForKeyPress():
-    for event in pygame.event.get():
-        if event.type == QUIT:      #event is quit 
-            terminate()
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:   #event is escape key
-                terminate()
-            else:
-                return event.key   #key found return with it
-    # no quit or key events in queue so return None    
-    return None
-
-    
-def showStartScreen():
-    titleFont = pygame.font.Font('freesansbold.ttf', 100)
-    titleSurf1 = titleFont.render('Wormy!', True, WHITE, DARKGREEN)
-    titleSurf2 = titleFont.render('Wormy!', True, GREEN)
-
-    degrees1 = 0
-    degrees2 = 0
-    
-#KRT 14/06/2012 rewrite event detection to deal with mouse use
-    pygame.event.get()  #clear out event queue
-    
-    while True:
-        DISPLAYSURF.fill(BGCOLOR)
-        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
-        rotatedRect1 = rotatedSurf1.get_rect()
-        rotatedRect1.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
-        DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
-
-        rotatedSurf2 = pygame.transform.rotate(titleSurf2, degrees2)
-        rotatedRect2 = rotatedSurf2.get_rect()
-        rotatedRect2.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
-        DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
-
-        drawPressKeyMsg()
-#KRT 14/06/2012 rewrite event detection to deal with mouse use
-        if checkForKeyPress():
-            return
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
-        degrees1 += 3 # rotate by 3 degrees each frame
-        degrees2 += 7 # rotate by 7 degrees each frame
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def getRandomLocation():
-    return {'x': random.randint(0, CELLWIDTH - 1), 'y': random.randint(0, CELLHEIGHT - 1)}
-
-
-def showGameOverScreen():
-    gameOverFont = pygame.font.Font('freesansbold.ttf', 150)
-    gameSurf = gameOverFont.render('Game', True, WHITE)
-    overSurf = gameOverFont.render('Over', True, WHITE)
-    gameRect = gameSurf.get_rect()
-    overRect = overSurf.get_rect()
-    gameRect.midtop = (WINDOWWIDTH / 2, 10)
-    overRect.midtop = (WINDOWWIDTH / 2, gameRect.height + 10 + 25)
-
-    DISPLAYSURF.blit(gameSurf, gameRect)
-    DISPLAYSURF.blit(overSurf, overRect)
-    drawPressKeyMsg()
-    pygame.display.update()
-    pygame.time.wait(500)
-#KRT 14/06/2012 rewrite event detection to deal with mouse use
-    pygame.event.get()  #clear out event queue 
-    while True:
-        if checkForKeyPress():
-            return
-#KRT 12/06/2012 reduce processor loading in gameover screen.
-        pygame.time.wait(100)
-
-def drawScore(score):
-    scoreSurf = BASICFONT.render('Score: %s' % (score), True, WHITE)
-    scoreRect = scoreSurf.get_rect()
-    scoreRect.topleft = (WINDOWWIDTH - 120, 10)
-    DISPLAYSURF.blit(scoreSurf, scoreRect)
-
-
-def drawWorm(gWormCoords):
-    for coord in gWormCoords:
-        x = coord['x'] * CELLSIZE
-        y = coord['y'] * CELLSIZE
-        wormSegmentRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
-        pygame.draw.rect(DISPLAYSURF, DARKGREEN, wormSegmentRect)
-        wormInnerSegmentRect = pygame.Rect(x + 4, y + 4, CELLSIZE - 8, CELLSIZE - 8)
-        pygame.draw.rect(DISPLAYSURF, GREEN, wormInnerSegmentRect)
-
-
-def drawApple(coord):
-    x = coord['x'] * CELLSIZE
-    y = coord['y'] * CELLSIZE
-    appleRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
-    pygame.draw.rect(DISPLAYSURF, RED, appleRect)
-
-
-def drawGrid():
-    for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
-    for y in range(0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
-
-
-if __name__ == '__main__':
-    main()
+    #will render the Character Selection Menu
+    def CharacterChRender(self, displaySurface):
+        displaySurface.blit(self.backImage,self.backImage.get_rect(topleft = (0,0)));
+        displaySurface.blit(self.image,self.image.get_rect(topleft = self.position));
+        
+        #Displays Ryu Sprites
+        displaySurface.blit(self.RyuImage,self.RyuImage.get_rect(topleft = self.RyuPosition));
+        displaySurface.blit(self.DarkRyuImage ,self.DarkRyuImage.get_rect(topleft = self.DarkRyuPosition));
+        displaySurface.blit(self.FlashRyuImage,self.FlashRyuImage.get_rect(topleft = self.FlashRyuPosition));
+        
+    #will render the Character Selection Menu's text
+    def renderCharacterChText(self, displaySurface):
+        #Set up Font Style
+        MenuFont = pygame.font.Font('freesansbold.ttf', 25);
+        
+        #Set the text to be written
+        RyuSurf = MenuFont.render('Ryu', True, WHITE);
+        DarkRyuSurf = MenuFont.render('Dark Ryu', True, WHITE);
+        FlashRyuSurf = MenuFont.render('Flash Ryu', True, WHITE);
+        
+        #make rectangles 
+        RyuRect = RyuSurf.get_rect();
+        DarkRyuRect = DarkRyuSurf.get_rect();
+        FlashRyuRect = FlashRyuSurf.get_rect();
+        
+        #set up Coords for display
+        RyuRect.midtop = (DISPLAY_WIDTH / 4, DISPLAY_HEIGHT/2 - 25);
+        DarkRyuRect.midtop = (3*(DISPLAY_WIDTH/4) - 20, DISPLAY_HEIGHT/2 - 25);
+        FlashRyuRect.midtop = (DISPLAY_WIDTH / 2 - 5, 3*(DISPLAY_HEIGHT/4) - 7);
+        
+        #Draw the text
+        displaySurface.blit(RyuSurf, RyuRect);
+        displaySurface.blit(DarkRyuSurf, DarkRyuRect);
+        displaySurface.blit(FlashRyuSurf, FlashRyuRect);
+        
+    #wilExitSurf = MenuFont.render('Exit', True, WHITE);l switch UV's to the Character Selections
+    def ChangeToChooseCh(self, displaySurface):
+        self.position = [25, 200];
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.ChooseChRect[0]));
+    #will switch UV's to the Normal Menu's
+    def ChangeToMenu(self, displaySurface):
+        self.position = [(DISPLAY_WIDTH/2) - 140, 50];
+        self.image = self.spriteSheet.subsurface(pygame.Rect(self.MenuRect[0]));
